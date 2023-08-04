4fc7b1b2c3b9c503076a2e4b7638170b08003135
Brendan <brendanjlee5@gmail.com>
2023-08-04 15:23:02 +0900

    update with ui

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// elements
+const buttons = document.querySelectorAll('button');
+
+// global variables
+let playerScore = 0;
+let computerScore = 0;
+let currRound = 1;
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) return "rock";
@@ -27,12 +35,69 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("your move");
-        let computerSelection = getComputerChoice();
-        alert(`Computer: ${computerSelection}, you ${playRound(playerSelection, computerSelection)}`);
-    }
+// updates game score and result on screen
+function updateScore(res) {
+    if (res == "win") playerScore++;
+    else if (res == 'lose') computerScore++;
+
+    const playerScoreboard = document.querySelector('.player-score');
+    const computeScoreboard = document.querySelector('.computer-score');
+
+    playerScoreboard.textContent = `${playerScore}`;
+    computeScoreboard.textContent = `${computerScore}`;
 }
 
-game();
+// update the selection element for player and computer
+function updateSelection(playerSelection, computerSelection) {
+    const playerChoice = document.querySelector('.player-choice');
+    console.log(playerChoice)
+    const computerChoice = document.querySelector('.computer-choice');
+
+    playerChoice.textContent = playerSelection;
+    computerChoice.textContent = computerSelection;
+}
+
+// update result of game
+function updateResult(res) {
+    const gameResult = document.querySelector('.game-result');
+    console.log(res)
+    gameResult.textContent = `you ${res}!`;
+}
+
+// update and check for rounds
+function updateRounds() {
+    ++currRound;
+    if (playerScore > 5 || computerScore > 5) {
+        alert("Game Over!");
+        window.location.reload();
+        return;
+    }
+    const roundCounter = document.querySelector('.round-counter');
+    roundCounter.textContent = `${currRound}`;
+}
+
+function buttonPressed(e) {
+    //console.log(this.classList.value);
+
+    // play a round
+    let playerSelection = 'rock';
+    if (this.classList.value == 'btn-paper') playerSelection = 'paper';
+    else if (this.classList.value == 'btn-scissors') playerSelection = 'scissors';
+    let computerSelection = getComputerChoice();
+
+    // update scoreboard 
+    let result = playRound(playerSelection, computerSelection)
+    updateScore(result);
+
+    // update game selection
+    updateSelection(playerSelection, computerSelection);
+
+    // update game result
+    updateResult(result);
+
+    // update
+    updateRounds();
+
+}
+
+buttons.forEach(btn => btn.addEventListener('click', buttonPressed));
